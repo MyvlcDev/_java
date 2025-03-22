998aa19b38fa10e3b011b73d0fb56bc6de0b0da5
Myvlc.dev <xsync80@myvlc.dev>
2025-03-22 16:51:40 +0100

    retosJava
    RetosJAVA

---
+++ retosProgramacion/src/retosProgramacion/prueba1.java
@@ -0,0 +1,14 @@
+package retosProgramacion;
+
+public class prueba1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		for(int i=0; i<=10; i++) 
+		
+		System.out.println("Hola mundo, prueba ");
+	}
+
+}
